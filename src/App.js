1282f6d5359787896dd16bbd86063bbd22f1c2b0
Akshay <akshay.d@reward360.co>
2024-05-16 23:42:11 +0530

    app - initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Navbar } from './components';
+
+function App() {
+  return (
+    <div>
+      {/* Navbar */}
+      <Navbar />
+      {/* Black Space */}
+    </div>
+  );
+}
+
+export default App;
